969d12d04a132a33e1d3194c1bd6ecfe37fd97fc
LarrySimiyu <larry.simiyu@gmail.com>
2019-12-05 10:30:29 -0800

    hash table push

--- src/hashtable.py
+++ src/hashtable.py
@@ -32,12 +32,12 @@ class HashTable:
 
         OPTIONAL STRETCH: Research and implement DJB2
         '''
-        hash_value = 5381
+        # hash_value = 5381
 
-        for char in key:
-            hash_value = ((hash_value << 5) + hash_value) + ord(char)
-        return hash_value
-        
+        # for char in key:
+        #     hash_value = ((hash_value << 5) + hash_value) + ord(char)
+        # return hash_value
+        pass
         
 
 
@@ -58,29 +58,21 @@ class HashTable:
 
         Fill this in.
         '''
-        if self.count / self.capacity > 0/7:
-            self.resize()
-        
-        if self.count / self.capacity < 0.2 and self.resized == True:
-            self.resize(0.5)
-
         index = self._hash_mod(key)
-        #save the value to that index if index is none
-
-        if not self.storage[index]:
-            self.storage[index] = LinkedPair(key, value)
-            self.count += 1
-
+        if self.storage[index] is not None:
+            current = self.storage[index]
+            while current is not None:
+                if current.key == key:
+                    current.value = value
+                    return
+                if current.next is None:
+                    current.next = LinkedPair(key, value)
+                    return
+                current = current.next
         else:
-            node = self.storage[index]
-            while node.next and node.key != key:
-                node = node.next
-            if node.key == key:
-                node.value = value
-            else:
-                node.next = LinkedPair(key, value)
-                self.count += 1
-
+            self.storage[index] = LinkedPair(key, value)
+       
+    
 
 
 
@@ -92,7 +84,18 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+
+        if self.storage[index] is None:
+            current = self.storage[index]
+            while current is not None:
+                if current.key == key:
+                    current = None
+                    return
+                current = current.next
+            return
+        self.storage[index] = None
+
 
 
     def retrieve(self, key):
@@ -103,7 +106,20 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+
+        if self.storage[index] is not None:
+            if self.storage[index].key == key:
+                return self.storage[index].value
+            else:
+                current = self.storage[index]
+                while current is not None:
+                    if current.key == key:
+                        return current.value
+                    current = current.next
+        else:
+            return None
+
 
 
     def resize(self):
@@ -113,7 +129,15 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        self.capacity *= 2
+        old_storage = self.storage
+        new_storage = [None] * self.capacity
+        self.storage = new_storage
+        for item in old_storage:
+            current = item
+            while current is not None:
+                self.insert(current.key, current.value)
+                current = current.next
 
 
 
